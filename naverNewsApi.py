df80f1f44c82e19ceb92bea4ef2807f7482bb647
kjhe1234 <kjhe1234@naver.com>
2024-04-26 11:44:22 +0900

    뉴스 검객 결과 출력 및 해당 신문사 링크 연결 기능 추가 함

--- naverNewsApi.py
+++ naverNewsApi.py
@@ -6,6 +6,8 @@ from PyQt5 import uic
 
 from naverSearchApi import *
 
+import webbrowser
+
 form_class = uic.loadUiType("ui/search.ui")[0]  # 외부에서 ui 불러오기  내부에서 불러오면 1
 
 class MainWindow(QMainWindow, form_class):
@@ -18,6 +20,8 @@ class MainWindow(QMainWindow, form_class):
         self.statusBar().showMessage("Naver News Search Application v1.0")
 
         self.search_btn.clicked.connect(self.searchbtn_clicked)
+        self.result_table.doubleClicked.connect(self.link_DoubleClicked)
+        # 테이블의 항목이 더블클릭되면 link_DoubleClicked 함수 호출
 
     def searchbtn_clicked(self):
         search = self.search_line.text()  # 사용자가 입력한 검색 키워드 가져오기
@@ -40,11 +44,30 @@ class MainWindow(QMainWindow, form_class):
         # 테이블의 첫 행(열 이름) 설정
         self.result_table.setHorizontalHeaderLabels(["기사제목","기사링크","게시시간"])
         # 각 칼럼의 넓이 지정(총 길이 790을 3등분)
-        self.result_table.setColumnWidth(0, 400)
-        self.result_table.setColumnWidth(1, 240)
-        self.result_table.setColumnWidth(2, 100)
+        self.result_table.setColumnWidth(0, 380)
+        self.result_table.setColumnWidth(1, 230)
+        self.result_table.setColumnWidth(2, 130)
         # 테이블에 출력되는 검색결과 수정 금지 기능 추가
         self.result_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        for i, news in enumerate(newsResult):  # i -> 0~9
+            newsTitle = news['title']  # 뉴스 제목
+            newsTitle = newsTitle.replace('&quot','').replace(';','').replace('<b>','').replace('</b>','')
+            newsLink = news['originallink'] # 뉴스의 오리지널 url 링크
+            newsDate = news['pubDate'] # 뉴스 게시일
+            newsDate = newsDate[0:25]
+
+            self.result_table.setItem(i, 0, QTableWidgetItem(newsTitle))
+            self.result_table.setItem(i, 1, QTableWidgetItem(newsLink))
+            self.result_table.setItem(i, 2, QTableWidgetItem(newsDate))
+
+    def link_DoubleClicked(self):   # 링크를 더블 클릭하면 호출되는 함수
+        selectedRow = self.result_table.currentRow()  # 현재 더블클릭하여 선택되어있는 행의 인덱스를 가져오기
+        selectedLink = self.result_table.item(selectedRow, 1).text() # 더블클릭한 셀의 텍스트를 가져오기
+        webbrowser.open(selectedLink)
+
+
+
 
 
 
